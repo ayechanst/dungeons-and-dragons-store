ef09da99556c84d2d56006ae8e25c1adba435edf
Aye Chan <ayechan.sant@gmail.com>
2023-04-13 07:56:37 -0700

    store

---
+++ pages/items.tsx
@@ -0,0 +1,20 @@
+export const generalStore = [
+  {
+    product: 'Snow Shoes',
+    price: '5 sp',
+    description:
+      'These warm shoes are lined with deer fur. They reach up to the ankles and their outsides\
+        are made of sealskin.',
+  },
+  {
+    product: 'Iron Sword',
+    price: '5 gp',
+    description:
+      'This iron sword is not the finest in Icewind Dale but will get the job done for sure.',
+  },
+  {
+    product: 'Plain Jane Rolex',
+    price: '100,000 gp',
+    description: 'A beautifully crafted time peice made by Gnomish craftsmen.',
+  },
+];
--- pages/store.tsx
+++ pages/store.tsx
@@ -1,7 +1,7 @@
 export default function Store() {
     return (
         <div>
-            <h1>item whats good</h1>
+            <h1>item whats good in da hood</h1>
         </div>
     )
 }
