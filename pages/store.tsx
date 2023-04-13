6496e27cc9e1ee28f3288cf14bdcb3ff398638f6
Aye Chan <ayechan.sant@gmail.com>
2023-04-13 09:48:39 -0700

    store is now one component

--- pages/store.tsx
+++ pages/store.tsx
@@ -2,32 +2,26 @@ import { useState } from 'react';
 import { RandomPicker } from './itemPicker';
 import { generalStore } from './items';
 
-export function Button() {
-  function onClick() {
-    const itemIndexes = RandomPicker(4);
+export default function Store() {
+  function handleClick() {
+    const itemIndexes = RandomPicker(5);
     //call the loadstore function
     console.log(itemIndexes);
   }
+
+  function Item() {
+    return (
+      <div>
+        <h4>item.product</h4>
+        <p>item.price</p>
+        <div>item.description</div>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <button onClick={onClick}>Generate Store</button>
+      <button onClick={handleClick}>Genertate Store</button>
     </div>
-  );
-}
-
-export function Item() {
-  return (
-    <div>
-      <h4>item.product</h4>
-      <p>item.price</p>
-      <div>item.description</div>
-    </div>
-  );
-}
-
-export default function Store() {
-  return (
-    <Button></Button>
-    // store
   );
 }
