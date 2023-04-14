cb6769eef919d3deeb86bdb2140f33bfa0227226
Aye Chan <ayechan.sant@gmail.com>
2023-04-14 11:38:40 -0700

    making component specifically for adverturing gear

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ export default function Store() {
     let randomNumbers = randomPicker(3);
     setItems(randomNumbers);
   }
+
   function handleMediumClick() {
     let randomNumbers = randomPicker(4);
     setItems(randomNumbers);
@@ -22,27 +23,30 @@ export default function Store() {
 
   return (
     <div>
-      <button
-        onClick={handleSmallClick}
-        className='class="middle none center rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-  data-ripple-light="true"'
-      >
-        Generate Small Store
-      </button>
-      <button
-        onClick={handleMediumClick}
-        className='class="middle none center rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-  data-ripple-light="true"'
-      >
-        Generate Medium Store
-      </button>
-      <button
-        onClick={handleBigClick}
-        className='class="middle none center rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-  data-ripple-light="true"'
-      >
-        Generate Big Store
-      </button>
+      <div className='flex gap-8'>
+        <button
+          onClick={handleSmallClick}
+          className='class="middle none center rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          data-ripple-light="true"'
+        >
+          Generate Small Store
+        </button>
+        <button
+          onClick={handleMediumClick}
+          className='class="middle none center rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          data-ripple-light="true"'
+        >
+          Generate Medium Store
+        </button>
+        <button
+          onClick={handleBigClick}
+          className='class="middle none center rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          data-ripple-light="true"'
+        >
+          Generate Big Store
+        </button>
+      </div>
+      <div className='font-extrabold'>Adveturing Gear</div>
       {items &&
         items.map((item) => {
           let newItem = gear[item];
