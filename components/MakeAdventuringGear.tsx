f770e07318c7fa525bf17db64248f9ee6ced3842
Aye Chan <ayechan.sant@gmail.com>
2023-04-17 13:13:37 -0700

    about to make a component for adventuring gear

--- components/MakeAdventuringGear.tsx
+++ components/MakeAdventuringGear.tsx
@@ -1,1 +1,21 @@
-export default function AdveturingGear() {}
+import { useState } from 'react';
+import MakeItem from '@/components/MakeItem';
+import { randomPicker } from '../library/helpers';
+import { gear } from '../library/items';
+
+export default function AdveturingGear() {
+  const [items, setItems] = useState<Array<number>>();
+  let randomNumbers = randomPicker(4);
+  setItems(randomNumbers);
+
+  return (
+    <div className='font-extrabold'>
+      <div>Adventuring Gear</div>
+      {items &&
+        items.map((item) => {
+          let newItem = gear[item];
+          return <MakeItem item={newItem} />;
+        })}
+    </div>
+  );
+}
