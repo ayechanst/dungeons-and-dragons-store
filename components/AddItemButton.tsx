6ee67240d323b49bdcdcab4a4cdb18760ae73930
Aye Chan <ayechan.sant@gmail.com>
2023-04-18 11:27:24 -0700

    form technically works

--- components/AddItemButton.tsx
+++ components/AddItemButton.tsx
@@ -1,13 +1,19 @@
 import { useState } from 'react';
 
-export default function AddItemButton() {
+interface FormPopUpProps {
+  onSubmit: (data: any) => void;
+}
+
+export default function AddItemButton({ onSubmit }: FormPopUpProps) {
   const [showForm, setShowForm] = useState(false);
 
   function handleClick() {
     setShowForm(!showForm);
   }
 
-  function handleSubmit() {
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
     console.log('the submit is being handled!');
   }
 
@@ -23,21 +29,40 @@ export default function AddItemButton() {
       {showForm && (
         <div className='fixed insert-0 z-50 flex items-center justify-center'>
           <div className='bg-white w-1/2 rounded-lg shadow-lg p-8'>
+            <div className='bold flex justify-center m-4'>New Item Form</div>
             <form onSubmit={handleSubmit}>
-              <label>Name</label>
               <input
                 type='text'
                 name='name'
                 id='name'
                 placeholder='Item Name'
+                className='m-1 border border-grey-400 px-2 py-2 w-full rounded-lg'
               ></input>
-              <label>Weight</label>
               <input
                 type='number'
                 name='weight'
                 id='weight'
                 placeholder='Item Weight'
+                className='m-1 border border-grey-400 px-2 py-2 w-full rounded-lg'
               ></input>
+              <input
+                type='number'
+                name='stock'
+                id='stock'
+                placeholder='Average Quantity'
+                className='m-1 border border-grey-400 px-2 py-2 w-full rounded-lg'
+              ></input>
+              <input
+                type='text'
+                name='description'
+                id='description'
+                placeholder='Item Description'
+                className='m-1 border border-grey-400 px-2 py-2 w-full rounded-lg'
+              ></input>
+              <button type='submit'>Submit</button>
+              <button type='button' onClick={handleClick}>
+                Close
+              </button>
             </form>
           </div>
         </div>
