d99eb000713fee4bd6e1cb9a9d205304bbed7fac
Chad Bro <ayechan.sant@gmail.com>
2023-09-11 12:18:20 -0700

    hero picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <>
-<div className="hero min-h-screen bg-base-200">
-  <div className="hero-content text-center">
-    <div className="max-w-md">
-      <h1 className="text-5xl font-bold">Hello there</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
-    </div>
-  </div>
-</div>
+      <div className="hero min-h-screen" style={{backgroundImage: 'url(/hero.jpg)'}}>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">D&D Store Generator</h1>
+            <p className="py-6">Choose what kind of store you like and awaaaayyyy we go!</p>
+            <div className="space-x-4">
+              <button className="btn btn-primary">General Store</button>
+              <button className="btn btn-primary">Armory</button>
+              <button className="btn btn-primary">Inn/Tavern</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
