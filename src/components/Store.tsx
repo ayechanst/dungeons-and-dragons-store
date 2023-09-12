88095f14be9e36f11e110ae7e47be7a5c79cb832
Chad Bro <ayechan.sant@gmail.com>
2023-09-11 18:52:27 -0700

    added items

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -1,7 +1,12 @@
+import Item from "../components/Item"
+
 export default function Store() {
     return (
         <>
-            <div>store</div>
+            <div className="navbar bg-base-100 flex justify-center">
+                <a className="btn btn-ghost normal-case text-xl">General Store</a>
+            </div>
+            <Item />
         </>
     )
 }
