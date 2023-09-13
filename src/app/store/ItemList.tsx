eaeefaed7e29790cc267fdc8ac2df6e66783c3c5
Chad Bro <ayechan.sant@gmail.com>
2023-09-12 22:39:31 -0700

    data can now be read from firebase

--- src/app/store/ItemList.tsx
+++ src/app/store/ItemList.tsx
@@ -1,19 +1,37 @@
+"use client"
 import Item from "../components/Item";
+import { collection, onSnapshot, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../firebase"
 
-async function getItems() {
-    const res = await fetch('http://localhost:4000/items');
-    return res.json();
+interface ItemData {
+    name: string;
+    description: string;
+    price: string;
 }
 
-export default async function ItemList() {
-    const items = await getItems();
+export default function ItemList() {
+    const [item, setItem] = useState<ItemData[]>([]);
+
+    useEffect(() => {
+        const q = query(collection(db, 'items'))
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            let itemsArray: ItemData[] = [];
+            querySnapshot.forEach((doc: any) => {
+                itemsArray.push({ ...doc.data(), id: doc.id })
+            })
+            setItem(itemsArray);
+            // where is items array?
+            return () => unsubscribe();
+        })
+    }, []);
 
     return (
         <>
             <div style={{ backgroundImage: 'url(/store.jpg)' }}>
                 <div className="flex justify-center py-20">
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                        {items.map((item: any, index: any) => (
+                        {item.map((item: any, index: any) => (
                             <div key={index}>
                                 <Item
                                     item={item}
